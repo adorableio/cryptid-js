4de344d5e6ccc19583b488edcd09298fec4ae98e
Zachery Moneypenny <whazzmaster@gmail.com>
2017-09-06 16:00:01 -0500

    Implements Tracker.send method with defaults

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import collectBrowserMetadata from './browser';
+import reqwest from 'reqwest';
+
+const CRYPTID_URL = "https://cryptid.adorable.io/api/events";
+
+function sendData(event, options = {}) {
+  reqwest({ data: event, ...options });
+}
+
+function generateRequestOptions() {
+  return {
+    url: CRYPTID_URL,
+    method: "post",
+    type: "json,"
+  };
+}
+
+function Tracker(trackerId, options) {
+  this.trackerId = trackerId;
+  this.options = options;
+  this.reqwest = reqwest;
+}
+
+Tracker.prototype.send = function(event) {
+  const metadata = collectBrowserMetadata();
+  let mergedEvent = {...metadata, ...event };
+  const config = generateRequestOptions();
+  let mergedConfig = {...config, ...this.options };
+  sendData(mergedEvent, mergedConfig);
+}
+
+export default Tracker;
