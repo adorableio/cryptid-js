62291032388a698777acc71fca437fe4486b5e00
Zachery Moneypenny <whazzmaster@gmail.com>
2017-09-15 13:39:21 -0500

    Pulls version from the package.json value

--- src-bin/cryptid.js
+++ src-bin/cryptid.js
@@ -1,10 +1,15 @@
+import path from 'path';
 import program from 'commander';
 
+let version = require(path.join(__dirname, '..', 'package.json'))
+                .version;
+
 program
-  .version('0.1.0')
-  .command('login', 'Login to cryptid')
+  .version(version)
+  .description('Admin interface to Cryptid Analytics')
+  .command('login', 'Login to Cryptid')
   .command('accounts', 'Interact with accounts')
   .command('products', 'Interact with products')
   .command('properties', 'Interact with properties')
-  .command('logout', 'Logout of cryptid')
+  .command('logout', 'Logout of Cryptid')
   .parse(process.argv);
