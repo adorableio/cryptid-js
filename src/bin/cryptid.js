c54cccac8238ad018bfebc6d95d294be8da6a747
Zachery Moneypenny <whazzmaster@gmail.com>
2017-10-27 10:22:00 -0500

    Fixes linting errors and removes commented code

--- src/bin/cryptid.js
+++ src/bin/cryptid.js
@@ -1,9 +1,5 @@
 import {loadHelp} from './help';
 
-//   .command('products', 'Product actions')
-//   .command('properties', 'Property actions')
-//   .command('query', 'Query the event store for a property')
-
 // eslint-disable-next-line no-unused-expressions
 require('yargs')
   .commandDir('commands')
